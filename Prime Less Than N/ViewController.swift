232f4062f155f968ddfa5c56fd82c504c4fa729d
five562 <liyiwu@Liyis-MBP.fios-router.home>
2018-03-06 23:26:46 -0500

    First Upload to Github

--- Prime Less Than N/ViewController.swift
+++ Prime Less Than N/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var primeList = [2,3]
+    
+    //Find all the primes less than a given number
+    func findAllPrime(ranNum: Int){
+        var newPrime = 3
+        var primeListLength = primeList.count
+        while ranNum > primeList[primeListLength-1] {
+            newPrime += 2
+            if newPrime > ranNum{
+                break
+            }else{
+                var check = 0
+                for i in primeList{
+                    if newPrime%i == 0{
+                        break
+                    }else{
+                        check += 1
+                    }
+                }
+                if check == primeListLength {
+                    primeList.append(newPrime)
+                    primeListLength += 1
+                }else{
+                }
+            }
+        }
+        print(primeList)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        findAllPrime(ranNum: 10000)
     }
 
     override func didReceiveMemoryWarning() {
