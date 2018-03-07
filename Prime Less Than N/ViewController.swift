e2659ad6c0853a87753207b2c0f616bae48ef607
five562 <liyiwu@liyis-mbp.fios-router.home>
2018-03-06 22:04:01 -0500

    Initial Commit

---
+++ Prime Less Than N/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime Less Than N
+//
+//  Created by Liyi Wu on 3/6/18.
+//  Copyright © 2018 Liyi Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
